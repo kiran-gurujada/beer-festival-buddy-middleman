ce7c104be5f84dda40d5f528c9837e19dd5112e1
Matt Oakley <matt.oakley@hashbanghq.com>
2016-12-02 15:34:19 +0000

    add og tags

--- config.rb
+++ config.rb
@@ -46,3 +46,16 @@ end
 activate :deploy do |deploy|
   deploy.deploy_method = :git
 end
+
+activate :ogp do |ogp|
+  #
+  # register namespace with default options
+  #
+  ogp.namespaces = {
+    fb: data.ogp.fb,
+    # from data/ogp/fb.yml
+    og: data.ogp.og
+    # from data/ogp/og.yml
+  }
+  ogp.base_url = 'http://www.beerfestivalbuddy.com/'
+end
